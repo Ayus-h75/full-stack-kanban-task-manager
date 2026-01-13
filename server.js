313b8c8365f0f15375042714ee60b0851cf8271d
Ayus-h75 <168477451+Ayus-h75@users.noreply.github.com>
2026-01-13 14:05:10 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './src/config/db.js';
+import authRoutes from './src/routes/authRoutes.js';
+import taskRoutes from './src/routes/taskRoutes.js';
+import { protect } from './src/middleware/authMiddleware.js';
+
+// Backend Server Setup
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', protect, taskRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
